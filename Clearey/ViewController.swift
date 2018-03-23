e498e93ff849243ad6910192a7774f0bc03bae48
Andresik Zoárd Manó <call.me.zeo@gmail.com>
2018-03-23 16:09:45 +0100

    Initial Commit

---
+++ Clearey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clearey
+//
+//  Created by Zeo on 2018. 03. 23..
+//  Copyright © 2018. Zeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
