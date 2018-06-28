51015c99c73b70292570df2d14151c2d684e19e4
Andresik Zoárd Manó <call.me.zeo@gmail.com>
2018-06-28 16:41:41 +0200

    Add CRUD and Core Data functions. Add search bar extension. Refractor code.

--- Clearey/Controllers/TodoViewController.swift
+++ Clearey/Controllers/TodoViewController.swift
@@ -7,20 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoViewController: UITableViewController {
 	
 	var itemArray = [Item]()
 	
 	let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-
+	
+	let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		print(dataFilePath!)
 
+		
+		
 		loadItems()
-		 
+		
 //		if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 //			itemArray = items
 //		}
@@ -28,7 +33,7 @@ class TodoViewController: UITableViewController {
 
 	}
 	
-	//MARK - TableView Datasource Method
+	//MARK: - TableView Datasource Method
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 	
@@ -49,7 +54,7 @@ class TodoViewController: UITableViewController {
 		return cell
 	}
 	
-	//MARK - TableView Delegate Methods
+	//MARK: - TableView Delegate Methods
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		
@@ -63,7 +68,7 @@ class TodoViewController: UITableViewController {
 	
 	}
 	
-	//MARK  - Add New Items
+	//MARK:  - Add New Items
 	
 	@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
 		
@@ -73,8 +78,9 @@ class TodoViewController: UITableViewController {
 		
 		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 			
-			let newItem = Item()
+			let newItem = Item(context: self.context)
 			newItem.title = TextField.text!
+			newItem.done = false
 			
 			self.itemArray.append(newItem)
 			
@@ -96,42 +102,65 @@ class TodoViewController: UITableViewController {
 	
 	func saveItems () {
 		
-		let encoder = PropertyListEncoder()
-		
 		do {
 			
-			let data = try encoder.encode(itemArray)
-			try data.write(to: dataFilePath!)
+			try context.save()
+		} catch {
+			
+			print("Error saving context, \(error)")
+		}
+
+		self.tableView.reloadData()
+
+		
+	}
+	
+	func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+		
+		do {
+		
+			itemArray = try context.fetch(request)
 			
 		} catch {
 			
-			print("Error encoding item array, \(error)")
-			
+			print("Error fetching data from context \(error)")
+		
 		}
-
+		
 		tableView.reloadData()
-
 		
 	}
 	
-	func loadItems() {
+	
+}
+
+//MARK: - Search Bar Methods
+
+extension TodoViewController: UISearchBarDelegate {
+
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
 		
-		if let data = try? Data(contentsOf: dataFilePath!) {
-			
-			let decoder = PropertyListDecoder()
-			do {
-				
-				itemArray = try decoder.decode([Item].self, from: data)
-			
-			} catch {
-			
-				print("Error decoding array, \(error)")
-			
+		let request : NSFetchRequest<Item> = Item.fetchRequest()
+		
+		request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+		
+		request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+		
+		loadItems(with: request)
+	}
+	
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+		if searchBar.text?.count == 0 {
+			loadItems()
+
+			DispatchQueue.main.async {
+				searchBar.resignFirstResponder()
 			}
+
+		}
 	}
-	
 
 }
 
-}
 
