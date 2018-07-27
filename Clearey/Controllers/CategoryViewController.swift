500b78882061af15bc443cfc219ed9615658bd3c
Andresik Zoárd Manó <call.me.zeo@gmail.com>
2018-07-27 17:08:52 +0200

    About to implement Category table view. Fallback point if I mess up.

---
+++ Clearey/Controllers/CategoryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryViewController.swift
+//  Clearey
+//
+//  Created by Zeo on 2018. 06. 28..
+//  Copyright © 2018. Zeo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+	}
+
+	//MARK: - TableView Datasource Methods
+	
+	
+	
+	//MARK: - Data Manipulation Methods
+
+	//MARK: - Add New Categories
+	
+	
+	@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+		
+	}
+	
+
+
+
+	//MARK: - TableView Delegate Methods
+
+
+}
